d0ac775fb97a7c379d405f1749ea88ec115bcfd4
TwistedVision518 <pranavpraveen0@gmail.com>
2026-08-08 09:26:28 +0000

    SystemUI: DynamicIsland: Update media popup icons
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/featurepods/media/ui/compose/MediaControlPopup.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/featurepods/media/ui/compose/MediaControlPopup.kt
@@ -173,6 +173,7 @@ fun MediaControlPopup(
             ) {
                 MediaActionButton(
                     action = model.previousAction,
+                    iconOverrideId = R.drawable.ic_skip_previous_filled,
                     containerColor = LocalContentColor.current.copy(alpha = 0.11f),
                     iconTint = LocalContentColor.current,
                 )
@@ -186,6 +187,7 @@ fun MediaControlPopup(
                 )
                 MediaActionButton(
                     action = model.nextAction,
+                    iconOverrideId = R.drawable.ic_skip_next_filled,
                     containerColor = LocalContentColor.current.copy(alpha = 0.11f),
                     iconTint = LocalContentColor.current,
                 )
@@ -578,8 +580,9 @@ private fun MediaActionButton(
     buttonSize: Dp = 54.dp,
     iconSize: Dp = 22.dp,
     emphasized: Boolean = false,
+    iconOverrideId: Int? = null,
 ) {
-    if (action == null || action.icon == null) {
+    if (action == null || (action.icon == null && iconOverrideId == null)) {
         Spacer(modifier = Modifier.size(buttonSize))
         return
     }
@@ -619,11 +622,18 @@ private fun MediaActionButton(
 >>>>>>> 00ce93bd92b7 (SystemUI: DynamicIsland: Media popup seekbar follow system seekbar)
         contentAlignment = Alignment.Center,
     ) {
-        UiIconView(
-            icon = UiIcon.Loaded(action.icon, contentDescription),
-            modifier = Modifier.size(iconSize).padding(start = if (emphasized) 1.dp else 0.dp),
-            tint = iconTint,
-        )
+        val actualIcon = if (iconOverrideId != null) {
+            UiIcon.Resource(iconOverrideId, contentDescription)
+        } else {
+            action.icon?.let { UiIcon.Loaded(it, contentDescription) }
+        }
+        if (actualIcon != null) {
+            UiIconView(
+                icon = actualIcon,
+                modifier = Modifier.size(iconSize).padding(start = if (emphasized) 1.dp else 0.dp),
+                tint = iconTint,
+            )
+        }
     }
 }
 
